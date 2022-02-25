9163f25d303188d21e2f1257717d7196b2ff9a8e
Tony Theodoropoulos <theodot@outlook.com>
2022-02-25 12:48:48 -0500

    Add project files.

---
+++ OverloadMethods/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace OverloadMethods
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.areaOutput = new System.Windows.Forms.Label();
+            this.value2Label = new System.Windows.Forms.Label();
+            this.value1Label = new System.Windows.Forms.Label();
+            this.value2Input = new System.Windows.Forms.TextBox();
+            this.value1Input = new System.Windows.Forms.TextBox();
+            this.areaButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // areaOutput
+            // 
+            this.areaOutput.BackColor = System.Drawing.Color.White;
+            this.areaOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.areaOutput.Location = new System.Drawing.Point(41, 300);
+            this.areaOutput.Name = "areaOutput";
+            this.areaOutput.Size = new System.Drawing.Size(284, 25);
+            this.areaOutput.TabIndex = 19;
+            this.areaOutput.Text = "...";
+            // 
+            // value2Label
+            // 
+            this.value2Label.AutoSize = true;
+            this.value2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value2Label.Location = new System.Drawing.Point(41, 245);
+            this.value2Label.Name = "value2Label";
+            this.value2Label.Size = new System.Drawing.Size(85, 25);
+            this.value2Label.TabIndex = 18;
+            this.value2Label.Text = "Value 2";
+            // 
+            // value1Label
+            // 
+            this.value1Label.AutoSize = true;
+            this.value1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value1Label.Location = new System.Drawing.Point(41, 203);
+            this.value1Label.Name = "value1Label";
+            this.value1Label.Size = new System.Drawing.Size(85, 25);
+            this.value1Label.TabIndex = 17;
+            this.value1Label.Text = "Value 1";
+            // 
+            // value2Input
+            // 
+            this.value2Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value2Input.Location = new System.Drawing.Point(131, 242);
+            this.value2Input.Name = "value2Input";
+            this.value2Input.Size = new System.Drawing.Size(63, 31);
+            this.value2Input.TabIndex = 16;
+            // 
+            // value1Input
+            // 
+            this.value1Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value1Input.Location = new System.Drawing.Point(131, 200);
+            this.value1Input.Name = "value1Input";
+            this.value1Input.Size = new System.Drawing.Size(63, 31);
+            this.value1Input.TabIndex = 15;
+            // 
+            // areaButton
+            // 
+            this.areaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.areaButton.Location = new System.Drawing.Point(200, 200);
+            this.areaButton.Name = "areaButton";
+            this.areaButton.Size = new System.Drawing.Size(125, 73);
+            this.areaButton.TabIndex = 14;
+            this.areaButton.Text = "Area";
+            this.areaButton.UseVisualStyleBackColor = true;
+            this.areaButton.Click += new System.EventHandler(this.areaButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(52, 30);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(256, 39);
+            this.titleLabel.TabIndex = 20;
+            this.titleLabel.Text = "Area Calculator";
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(91, 83);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(121, 20);
+            this.instructionLabel.TabIndex = 21;
+            this.instructionLabel.Text = "instructionLabel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(369, 381);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.areaOutput);
+            this.Controls.Add(this.value2Label);
+            this.Controls.Add(this.value1Label);
+            this.Controls.Add(this.value2Input);
+            this.Controls.Add(this.value1Input);
+            this.Controls.Add(this.areaButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Area Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label areaOutput;
+        private System.Windows.Forms.Label value2Label;
+        private System.Windows.Forms.Label value1Label;
+        private System.Windows.Forms.TextBox value2Input;
+        private System.Windows.Forms.TextBox value1Input;
+        private System.Windows.Forms.Button areaButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label instructionLabel;
+    }
+}
+
---
+++ OverloadMethods/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverloadMethods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            instructionLabel.Text = "Enter: \n\n1 value for a circle\n2 values for a rectangle";
+        }
+
+        private void areaButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
